966cf8a97a948023617edc76d97ce3ac605af9b5
jikkai <jikkai@users.noreply.github.com>
2026-07-09 10:06:53 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-7221/preset-sheets-core/main.js
@@ -0,0 +1,283 @@
+import {
+  UniverSheetsConditionalFormattingPreset,
+  UniverSheetsCorePreset,
+  UniverSheetsDataValidationPreset,
+  UniverSheetsDrawingPreset,
+  UniverSheetsFilterPreset,
+  UniverSheetsFindReplacePreset,
+  UniverSheetsHyperLinkPreset,
+  UniverSheetsNotePreset,
+  UniverSheetsSortPreset,
+  UniverSheetsTablePreset,
+  UniverSheetsThreadCommentPreset,
+  zh_CN_default,
+  zh_CN_default2 as zh_CN_default3,
+  zh_CN_default3 as zh_CN_default4,
+  zh_CN_default4 as zh_CN_default6,
+  zh_CN_default5 as zh_CN_default7,
+  zh_CN_default6 as zh_CN_default8,
+  zh_CN_default7 as zh_CN_default9,
+  zh_CN_default8 as zh_CN_default10,
+  zh_CN_default9 as zh_CN_default11
+} from "../chunk-FD26MOJF.js";
+import "../chunk-UP3Y5QBJ.js";
+import "../chunk-C2YNMFJH.js";
+import "../chunk-W3B46X4G.js";
+import "../chunk-XS7CT7NS.js";
+import "../chunk-XORXCH5U.js";
+import "../chunk-VXEVKRCX.js";
+import "../chunk-IZVXBBG6.js";
+import {
+  zh_CN_default as zh_CN_default2,
+  zh_CN_default2 as zh_CN_default5
+} from "../chunk-A3LR6BSI.js";
+import {
+  createUniver
+} from "../chunk-NBYYXNCB.js";
+import "../chunk-XGPDREVR.js";
+import "../chunk-XNVZHWKN.js";
+import "../chunk-E6NV4XUQ.js";
+import "../chunk-WPH7MSAJ.js";
+import "../chunk-QMEJ6ZKY.js";
+import "../chunk-IKYRH57R.js";
+import "../chunk-7ZIONOSY.js";
+import "../chunk-5RDHIJMY.js";
+import "../chunk-L7FJZGIP.js";
+import "../chunk-NTBTAPWX.js";
+import "../chunk-PEAFN7ZQ.js";
+import "../chunk-SRL2N57D.js";
+import "../chunk-QZVDG6QU.js";
+import "../chunk-4COF32XS.js";
+import "../chunk-QPFCOL32.js";
+import "../chunk-MX54RWPE.js";
+import "../chunk-AKLDEHKU.js";
+import "../chunk-2RXBLMEL.js";
+import "../chunk-DJK32BKN.js";
+import "../chunk-LI6UXASZ.js";
+import {
+  FolderIcon,
+  IMenuManagerService,
+  IconManager
+} from "../chunk-5WPUL4JN.js";
+import "../chunk-DYNNSAVU.js";
+import "../chunk-CPLPYTNU.js";
+import "../chunk-SNSWR7JB.js";
+import "../chunk-QNDLTLR6.js";
+import "../chunk-2FVEG47S.js";
+import "../chunk-GNAKMJK7.js";
+import "../chunk-MESKVJUQ.js";
+import {
+  SetRangeValuesMutation,
+  SetRangeValuesUndoMutationFactory,
+  SetWorksheetColumnCountMutation,
+  SetWorksheetColumnCountUndoMutationFactory,
+  SetWorksheetRowCountMutation,
+  SetWorksheetRowCountUndoMutationFactory
+} from "../chunk-JZ2LFV55.js";
+import {
+  ICommandService,
+  IUndoRedoService,
+  IUniverInstanceService,
+  Inject,
+  Injector,
+  Plugin,
+  covertCellValues,
+  default_default,
+  mergeLocales,
+  sequenceExecute
+} from "../chunk-YAGO4HXX.js";
+import "../chunk-EQ2B2W73.js";
+import {
+  __decorateClass,
+  __decorateParam,
+  __publicField
+} from "../chunk-HECJ2TYE.js";
+
+// src/preset-sheets-core/custom-plugin/import-csv-button.ts
+function waitUserSelectCSVFile(onSelect) {
+  return new Promise((resolve) => {
+    const input = document.createElement("input");
+    input.type = "file";
+    input.accept = ".csv";
+    input.click();
+    input.onchange = () => {
+      var _a;
+      const file = (_a = input.files) == null ? void 0 : _a[0];
+      if (!file) {
+        return;
+      }
+      const reader = new FileReader();
+      reader.onload = () => {
+        const text = reader.result;
+        if (typeof text !== "string") {
+          return;
+        }
+        const rows = text.split(/\r\n|\n/);
+        const data = rows.map((line) => line.split(","));
+        const colsCount = data.reduce((max, row) => Math.max(max, row.length), 0);
+        const result = onSelect({
+          data,
+          colsCount,
+          rowsCount: data.length
+        });
+        resolve(result);
+      };
+      reader.readAsText(file);
+    };
+  });
+}
+var ImportCSVButtonPlugin = class extends Plugin {
+  constructor(_config, _injector, _menuManagerService, _commandService, _iconManager) {
+    super();
+    __publicField(this, "_injector", _injector);
+    __publicField(this, "_menuManagerService", _menuManagerService);
+    __publicField(this, "_commandService", _commandService);
+    __publicField(this, "_iconManager", _iconManager);
+  }
+  /**
+   * The first lifecycle of the plugin mounted on the Univer instance,
+   * the Univer business instance has not been created at this time.
+   * The plugin should add its own module to the dependency injection system at this lifecycle.
+   * It is not recommended to initialize the internal module of the plugin outside this lifecycle.
+   */
+  onStarting() {
+    this.disposeWithMe(
+      this._iconManager.register("FolderIcon2", FolderIcon)
+    );
+    const buttonId = "import-csv-button";
+    const command = {
+      type: 1 /* OPERATION */,
+      id: buttonId,
+      handler: (accessor) => {
+        const univerInstanceService = accessor.get(IUniverInstanceService);
+        const commandService = accessor.get(ICommandService);
+        const undoRedoService = accessor.get(IUndoRedoService);
+        const worksheet = univerInstanceService.getCurrentUnitOfType(2 /* UNIVER_SHEET */).getActiveSheet();
+        const unitId = worksheet.getUnitId();
+        const subUnitId = worksheet.getSheetId();
+        return waitUserSelectCSVFile(({ data, rowsCount, colsCount }) => {
+          const redoMutations = [];
+          const undoMutations = [];
+          const setRowCountMutationRedoParams = {
+            unitId,
+            subUnitId,
+            rowCount: rowsCount
+          };
+          const setRowCountMutationUndoParams = SetWorksheetRowCountUndoMutationFactory(
+            accessor,
+            setRowCountMutationRedoParams
+          );
+          redoMutations.push({ id: SetWorksheetRowCountMutation.id, params: setRowCountMutationRedoParams });
+          undoMutations.push({ id: SetWorksheetRowCountMutation.id, params: setRowCountMutationUndoParams });
+          const setColumnCountMutationRedoParams = {
+            unitId,
+            subUnitId,
+            columnCount: colsCount
+          };
+          const setColumnCountMutationUndoParams = SetWorksheetColumnCountUndoMutationFactory(
+            accessor,
+            setColumnCountMutationRedoParams
+          );
+          redoMutations.push({ id: SetWorksheetColumnCountMutation.id, params: setColumnCountMutationRedoParams });
+          undoMutations.unshift({ id: SetWorksheetColumnCountMutation.id, params: setColumnCountMutationUndoParams });
+          const cellValue = covertCellValues(data, {
+            startColumn: 0,
+            // start column index
+            startRow: 0,
+            // start row index
+            endColumn: colsCount - 1,
+            // end column index
+            endRow: rowsCount - 1
+            // end row index
+          });
+          const setRangeValuesMutationRedoParams = {
+            unitId,
+            subUnitId,
+            cellValue
+          };
+          const setRangeValuesMutationUndoParams = SetRangeValuesUndoMutationFactory(
+            accessor,
+            setRangeValuesMutationRedoParams
+          );
+          redoMutations.push({ id: SetRangeValuesMutation.id, params: setRangeValuesMutationRedoParams });
+          undoMutations.unshift({ id: SetRangeValuesMutation.id, params: setRangeValuesMutationUndoParams });
+          const result = sequenceExecute(redoMutations, commandService);
+          if (result.result) {
+            undoRedoService.pushUndoRedo({
+              unitID: unitId,
+              undoMutations,
+              redoMutations
+            });
+            return true;
+          }
+          return false;
+        });
+      }
+    };
+    const menuItemFactory = () => ({
+      id: buttonId,
+      title: "Import CSV",
+      tooltip: "Import CSV",
+      icon: "FolderIcon2",
+      // icon name
+      type: 0 /* BUTTON */
+    });
+    this._menuManagerService.mergeMenu({
+      ["ribbon.start.others" /* OTHERS */]: {
+        [buttonId]: {
+          order: 10,
+          menuItemFactory
+        }
+      }
+    });
+    this._commandService.registerCommand(command);
+  }
+};
+__publicField(ImportCSVButtonPlugin, "pluginName", "import-csv-plugin");
+ImportCSVButtonPlugin = __decorateClass([
+  __decorateParam(1, Inject(Injector)),
+  __decorateParam(2, Inject(IMenuManagerService)),
+  __decorateParam(3, Inject(ICommandService)),
+  __decorateParam(4, Inject(IconManager))
+], ImportCSVButtonPlugin);
+var import_csv_button_default = ImportCSVButtonPlugin;
+
+// src/preset-sheets-core/main.ts
+var { univer, univerAPI } = createUniver({
+  locale: "zhCN" /* ZH_CN */,
+  locales: {
+    zhCN: mergeLocales(
+      zh_CN_default2,
+      zh_CN_default4,
+      zh_CN_default,
+      zh_CN_default3,
+      zh_CN_default5,
+      zh_CN_default6,
+      zh_CN_default7,
+      zh_CN_default8,
+      zh_CN_default9,
+      zh_CN_default10,
+      zh_CN_default11
+    )
+  },
+  theme: default_default,
+  presets: [
+    UniverSheetsCorePreset(),
+    UniverSheetsDrawingPreset(),
+    UniverSheetsConditionalFormattingPreset(),
+    UniverSheetsFilterPreset(),
+    UniverSheetsHyperLinkPreset(),
+    UniverSheetsDataValidationPreset(),
+    UniverSheetsFindReplacePreset(),
+    UniverSheetsNotePreset(),
+    UniverSheetsSortPreset(),
+    UniverSheetsTablePreset(),
+    UniverSheetsThreadCommentPreset()
+  ],
+  plugins: [
+    import_csv_button_default
+  ]
+});
+univerAPI.createWorkbook({ name: "Test Sheet" });
+window.univer = univer;
+window.univerAPI = univerAPI;
---
+++ pr-preview/pr-7221/react16/very-lazy-JXPVT6SH.js
@@ -0,0 +1,62 @@
+import {
+  UniverActionRecorderPlugin
+} from "./chunk-I53DPVWE.js";
+import {
+  UniverSheetsFindReplacePlugin
+} from "./chunk-UP3Y5QBJ.js";
+import {
+  UniverSheetsSortUIPlugin
+} from "./chunk-XS7CT7NS.js";
+import {
+  UniverSheetsCrosshairHighlightPlugin
+} from "./chunk-FTFAJXH2.js";
+import {
+  UniverDebuggerPlugin
+} from "./chunk-BBRO7YNA.js";
+import {
+  UniverWatermarkPlugin
+} from "./chunk-K6REBI5O.js";
+import "./chunk-LYQIZRLR.js";
+import {
+  loadDebuggerLocale
+} from "./chunk-II2AZQAX.js";
+import "./chunk-QMEJ6ZKY.js";
+import {
+  UniverSheetsHyperLinkUIPlugin
+} from "./chunk-7ZIONOSY.js";
+import "./chunk-5RDHIJMY.js";
+import "./chunk-PEAFN7ZQ.js";
+import "./chunk-SRL2N57D.js";
+import "./chunk-QZVDG6QU.js";
+import "./chunk-2RXBLMEL.js";
+import "./chunk-DJK32BKN.js";
+import "./chunk-5WPUL4JN.js";
+import "./chunk-QNDLTLR6.js";
+import "./chunk-MESKVJUQ.js";
+import "./chunk-JZ2LFV55.js";
+import "./chunk-YAGO4HXX.js";
+import "./chunk-EQ2B2W73.js";
+import "./chunk-HECJ2TYE.js";
+
+// src/sheets/very-lazy.ts
+var IS_E2E = false;
+function getVeryLazyPlugins() {
+  const plugins = [
+    [UniverActionRecorderPlugin],
+    [UniverSheetsHyperLinkUIPlugin],
+    [UniverSheetsSortUIPlugin],
+    [UniverSheetsCrosshairHighlightPlugin],
+    [UniverSheetsFindReplacePlugin],
+    [UniverWatermarkPlugin]
+  ];
+  if (!IS_E2E) {
+    plugins.push([UniverDebuggerPlugin, {
+      fabEntryUnitType: 2 /* UNIVER_SHEET */,
+      localeLoader: loadDebuggerLocale
+    }]);
+  }
+  return plugins;
+}
+export {
+  getVeryLazyPlugins as default
+};
