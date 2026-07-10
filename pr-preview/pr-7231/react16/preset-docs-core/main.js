af4c26596fa847d2f611ed4ede12262ce91763f2
DR-Univer <DR-Univer@users.noreply.github.com>
2026-07-10 18:07:22 +0000

    Deploy preview for PR  🛫

---
+++ pr-preview/pr-7231/react16/preset-docs-core/main.js
@@ -0,0 +1,182 @@
+import {
+  UniverDocsHyperLinkPlugin,
+  UniverDocsHyperLinkUIPlugin,
+  UniverDocsThreadCommentUIPlugin
+} from "../chunk-ORX7NITO.js";
+import {
+  UniverThreadCommentUIPlugin
+} from "../chunk-ANSCH7RJ.js";
+import {
+  createUniver
+} from "../chunk-DNZSMGIY.js";
+import {
+  UniverDocsDrawingUIPlugin
+} from "../chunk-E52IRQ5F.js";
+import {
+  DEFAULT_DOCUMENT_DATA_SIMPLE
+} from "../chunk-C4ZBMQOC.js";
+import "../chunk-YYWFEPEY.js";
+import {
+  UniverNetworkPlugin
+} from "../chunk-RRTI72XC.js";
+import "../chunk-S7BGUSP2.js";
+import "../chunk-4QKKA4K3.js";
+import {
+  UniverDocsDrawingPlugin,
+  UniverDrawingPlugin,
+  UniverDrawingUIPlugin
+} from "../chunk-JCLKBUN7.js";
+import {
+  UniverDocsPlugin,
+  UniverDocsUIPlugin
+} from "../chunk-2NOALENK.js";
+import "../chunk-LI6UXASZ.js";
+import {
+  UniverUIPlugin
+} from "../chunk-KGQPEUSI.js";
+import "../chunk-NJAR46YV.js";
+import {
+  zh_CN_default as zh_CN_default2,
+  zh_CN_default2 as zh_CN_default3,
+  zh_CN_default3 as zh_CN_default4
+} from "../chunk-DBRQB4K6.js";
+import {
+  zh_CN_default as zh_CN_default6,
+  zh_CN_default2 as zh_CN_default7
+} from "../chunk-SNSWR7JB.js";
+import {
+  zh_CN_default,
+  zh_CN_default2 as zh_CN_default5,
+  zh_CN_default3 as zh_CN_default8
+} from "../chunk-GNAKMJK7.js";
+import "../chunk-HWBBMCPW.js";
+import {
+  UniverFormulaEnginePlugin
+} from "../chunk-2ZHNGVC5.js";
+import {
+  IImageIoService,
+  UniverRenderEnginePlugin,
+  default_default,
+  mergeLocales
+} from "../chunk-6Z6YV3AU.js";
+import "../chunk-EQ2B2W73.js";
+import "../chunk-HECJ2TYE.js";
+
+// ../presets/packages/preset-docs-core/src/preset.ts
+function UniverDocsCorePreset(config = {}) {
+  const {
+    container = "app",
+    header,
+    footer,
+    toolbar,
+    ribbonType,
+    menu,
+    contextMenu,
+    disableAutoFocus
+  } = config;
+  return {
+    plugins: [
+      UniverNetworkPlugin,
+      UniverDocsPlugin,
+      UniverRenderEnginePlugin,
+      [UniverUIPlugin, {
+        container,
+        header,
+        footer,
+        toolbar,
+        ribbonType,
+        menu,
+        contextMenu,
+        disableAutoFocus
+      }],
+      UniverDocsUIPlugin,
+      UniverFormulaEnginePlugin
+    ]
+  };
+}
+
+// ../presets/packages/preset-docs-core/src/locales/zh-CN.ts
+var zh_CN_default9 = Object.assign(
+  {},
+  zh_CN_default,
+  zh_CN_default5,
+  zh_CN_default8
+);
+
+// ../presets/packages/preset-docs-drawing/src/preset.ts
+function UniverDocsDrawingPreset(config = {}) {
+  const { collaboration = false } = config;
+  return {
+    plugins: [
+      [UniverDrawingPlugin, { override: collaboration ? [[IImageIoService, null]] : [] }],
+      UniverDrawingUIPlugin,
+      UniverDocsDrawingPlugin,
+      UniverDocsDrawingUIPlugin
+    ].filter((v) => !!v)
+  };
+}
+
+// ../presets/packages/preset-docs-drawing/src/locales/zh-CN.ts
+var zh_CN_default10 = Object.assign(
+  {},
+  zh_CN_default2,
+  zh_CN_default6
+);
+
+// ../presets/packages/preset-docs-hyper-link/src/preset.ts
+function UniverDocsHyperLinkPreset() {
+  return {
+    plugins: [
+      UniverDocsHyperLinkPlugin,
+      UniverDocsHyperLinkUIPlugin
+    ].filter((v) => !!v)
+  };
+}
+
+// ../presets/packages/preset-docs-hyper-link/src/locales/zh-CN.ts
+var zh_CN_default11 = Object.assign(
+  {},
+  zh_CN_default3
+);
+
+// ../presets/packages/preset-docs-thread-comment/src/preset.ts
+function UniverDocsThreadCommentPreset(_config = {}) {
+  const plugins = [
+    UniverThreadCommentUIPlugin,
+    UniverDocsThreadCommentUIPlugin
+  ];
+  return { plugins };
+}
+
+// ../presets/packages/preset-docs-thread-comment/src/locales/zh-CN.ts
+var zh_CN_default12 = Object.assign(
+  {},
+  zh_CN_default4,
+  zh_CN_default7
+);
+
+// src/preset-docs-core/main.ts
+var { univer, univerAPI } = createUniver({
+  locale: "zhCN" /* ZH_CN */,
+  locales: {
+    ["zhCN" /* ZH_CN */]: mergeLocales(
+      zh_CN_default9,
+      zh_CN_default10,
+      zh_CN_default11,
+      zh_CN_default12
+    )
+  },
+  theme: default_default,
+  logLevel: 4 /* VERBOSE */,
+  presets: [
+    UniverDocsCorePreset({
+      container: "app"
+    }),
+    UniverDocsDrawingPreset(),
+    UniverDocsHyperLinkPreset(),
+    UniverDocsThreadCommentPreset()
+  ]
+});
+univer.createUnit(1 /* UNIVER_DOC */, DEFAULT_DOCUMENT_DATA_SIMPLE);
+window.univer = univer;
+window.univerAPI = univerAPI;
---
+++ pr-preview/pr-7231/react16/sheets/worker.js
@@ -0,0 +1,41 @@
+import {
+  zh_CN_default
+} from "../chunk-ZS3JSJNT.js";
+import "../chunk-DBRQB4K6.js";
+import "../chunk-CPLPYTNU.js";
+import "../chunk-SNSWR7JB.js";
+import {
+  UniverSheetsFilterPlugin
+} from "../chunk-J4SJ2JVM.js";
+import "../chunk-2FVEG47S.js";
+import "../chunk-GNAKMJK7.js";
+import {
+  UniverRemoteSheetsFormulaPlugin
+} from "../chunk-HWBBMCPW.js";
+import {
+  UniverFormulaEnginePlugin,
+  UniverRPCWorkerThreadPlugin,
+  UniverSheetsPlugin
+} from "../chunk-2ZHNGVC5.js";
+import {
+  Univer
+} from "../chunk-6Z6YV3AU.js";
+import "../chunk-EQ2B2W73.js";
+import "../chunk-HECJ2TYE.js";
+
+// src/sheets/worker.ts
+var univer = new Univer({
+  locale: "zhCN" /* ZH_CN */,
+  logLevel: 4 /* VERBOSE */,
+  locales: {
+    ["zhCN" /* ZH_CN */]: zh_CN_default
+  }
+});
+univer.registerPlugins([
+  [UniverSheetsPlugin, { onlyRegisterFormulaRelatedMutations: true }],
+  [UniverFormulaEnginePlugin],
+  [UniverRPCWorkerThreadPlugin],
+  [UniverRemoteSheetsFormulaPlugin],
+  [UniverSheetsFilterPlugin]
+]);
+self.univer = univer;
